ce0911798227dda7b45c47277d7a646f3aba4367
Benjamin Reid <benjamin@nouveller.com>
2016-10-08 14:00:28 +0100

    Create very basic Board and Pad components, this also sets out a structure for the components and CSS

---
+++ src/components/Pad.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../css/Pad.css';
+
+class Pad extends Component {
+  render() {
+    return (
+      <div className="Pad">
+        <h2>Pad</h2>
+      </div>
+    );
+  }
+}
+
+export default Pad;
