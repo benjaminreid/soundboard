5082d965fd25391dd26ec0af120dd983affcb8f2
Benjamin Reid <benjamin@nouveller.com>
2016-10-12 21:38:49 +0100

    Load in soundmanager and basic sounds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import Board from './components/Board';
+import { soundManager } from './../node_modules/soundmanager2/script/soundmanager2';
 
 class App extends Component {
+  componentWillMount() {
+    soundManager.setup({
+      onready: () => {
+        const intro = soundManager.createSound({
+          id: 'intro',
+          url: `${process.env.PUBLIC_URL}/audio/intro.m4a`,
+        });
+
+        const outro = soundManager.createSound({
+          id: 'outro',
+          url: `${process.env.PUBLIC_URL}/audio/outro.m4a`,
+        });
+      },
+    });
+  }
+
   render() {
     return (
       <div>
